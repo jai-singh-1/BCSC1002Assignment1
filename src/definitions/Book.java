f26582f085140e5f8c84c3ac3d96d5e484e55991
jai_singh_1 <jai.singh_cs19@gla.ac.in>
2020-08-26 15:58:54 +0530

    Add constructor to Book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -12,4 +12,10 @@ public class Book {
     private String nameOfTheBook;
     private String nameOfTheAuthorOfTheBook;
     private String thirteenDigitISBNNumberOfTheBook;
+
+    public Book(String nameOfTheBook, String nameOfTheAuthorOfTheBook, String thirteenDigitISBNNumberOfTheBook){
+        this.nameOfTheBook = nameOfTheBook;
+        this.nameOfTheAuthorOfTheBook = nameOfTheAuthorOfTheBook;
+        this.thirteenDigitISBNNumberOfTheBook = thirteenDigitISBNNumberOfTheBook;
+    }
 }
