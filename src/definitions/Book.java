af94248d8a24b436a9e7cb03f6ada2b7b9afce7c
jai_singh_1 <jai.singh_cs19@gla.ac.in>
2020-08-30 09:10:46 +0530

    add a constructor method() to Book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -12,4 +12,9 @@ public class Book {
     private String bookName;
     private String authorName;
     private String ISBNNumber;
+    public Book(String bookName, String authorName, String ISBNNumber) {
+        this.bookName = bookName;
+        this.authorName = authorName;
+        this.ISBNNumber = ISBNNumber;
+    }
 }
