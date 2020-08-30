7926b0c3bbe9907a3248a58603957dced36728c9
jai_singh_1 <jai.singh_cs19@gla.ac.in>
2020-08-30 09:26:47 +0530

    add a consturctor method() to Student.java

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -14,4 +14,14 @@ public class Student {
     private long universityRollNumber;
     private int numberOfBooksIssued;
     private Book[] namesOfTheBooksIssued;
+
+    public Student(String nameOfTheStudent, long universityRollNumber, int numberOfBooksIssued) {
+        this.nameOfTheStudent = nameOfTheStudent;
+        this.universityRollNumber = universityRollNumber;
+        this.numberOfBooksIssued = numberOfBooksIssued;
+        this.namesOfTheBooksIssued = new Book[numberOfBooksIssued];
+        for (int index = 0; index < namesOfTheBooksIssued.length; index++) {
+            namesOfTheBooksIssued[index] = new Book("Book" + (index + 1), "Author" + (index + 1), "1234567891234");
+        }
+    }
 }
