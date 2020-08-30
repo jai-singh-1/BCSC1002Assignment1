7ded58b10a9a121c578a6680c20b5709cefef211
jai_singh_1 <jai.singh_cs19@gla.ac.in>
2020-08-30 09:12:34 +0530

    add getter amd setter method() to Book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -12,9 +12,35 @@ public class Book {
     private String bookName;
     private String authorName;
     private String ISBNNumber;
+
     public Book(String bookName, String authorName, String ISBNNumber) {
         this.bookName = bookName;
         this.authorName = authorName;
         this.ISBNNumber = ISBNNumber;
     }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getISBNNumber() {
+        return ISBNNumber;
+    }
+
+    public void setISBNNumber(String ISBNNumber) {
+        this.ISBNNumber = ISBNNumber;
+    }
 }
+
