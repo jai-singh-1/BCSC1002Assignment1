75b0d0802e0eae48b3736ae2b6a0e2f94c0b5f6d
jai_singh_1 <jai.singh_cs19@gla.ac.in>
2020-08-30 09:22:01 +0530

    add equal and hashcode method() to Library.java

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -30,6 +30,18 @@ public class Library {
     public String toString() {
         return Arrays.toString(currentlyAvailableBooks);
     }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Library library = (Library) o;
+        return Arrays.equals(getCurrentlyAvailableBooks(), library.getCurrentlyAvailableBooks());
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(getCurrentlyAvailableBooks());
+    }
 }
 
 
